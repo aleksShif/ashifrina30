bb84dfb8f0cde48eea47f2f1f914dedeb0812e97
aleksShif <sasha.shifrina05@gmail.com>
2022-09-28 14:28:37 -0400

    loop to populate dictionary

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -1,7 +1,9 @@
 krewes = "2$$$A$$$B@@@7$$$C$$$D@@@8$$$E$$$F"
 
-info = krewes.split("@@@")
-for i in range(len(info)):
-    devo = info[i].split("$$$")
-    print(devo)
-print(info)
+def dev_duck(stKrew):
+    di = {}
+    info = krewes.split("@@@")
+    for i in range(len(info)):
+        devo = tuple(info[i].split("$$$"))
+        di[devo[0]] = devo[1] + ' ' + devo[2]
+print(di)
